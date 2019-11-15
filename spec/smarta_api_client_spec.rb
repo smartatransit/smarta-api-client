b92b1975caba4b8c695db3a7f4ae40c58e7fd11e
trillnye <8289478+trillnye@users.noreply.github.com>
2019-11-15 13:20:21 -0500

    Remove fake first test

--- spec/smarta_api_client_spec.rb
+++ spec/smarta_api_client_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe SmartaApiClient do
   it 'has a version number' do
     expect(SmartaApiClient::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
